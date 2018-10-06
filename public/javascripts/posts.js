46963649ea44e5970bd6053ef5312b5baae1a103
louillustrator <lou.illustrator@gmail.com>
2018-10-06 14:12:45 +0100

    added some CSS to the post page to style like the image. I've used Ubuntu font from google

--- public/javascripts/posts.js
+++ public/javascripts/posts.js
@@ -1,39 +1,41 @@
 var postsOnPage = 0;
 
-$(function () {
-    $('#loadmore').click(() => {
-        loadPosts();
-    });
-
+$(function() {
+  $("#loadmore").click(() => {
     loadPosts();
+  });
+
+  loadPosts();
 });
 
 function loadPosts() {
-    var $postholder = $('#postholder');
+  var $postholder = $("#postholder");
 
-    $.ajax({
-        type: 'GET',
-        url: `/api/posts/?howMany=5&offset=${postsOnPage}`,
-        success: function (data) {
-            data.posts.forEach((post) => {
-                $postholder.append(`
+  $.ajax({
+    type: "GET",
+    url: `/api/posts/?howMany=5&offset=${postsOnPage}`,
+    success: function(data) {
+      data.posts.forEach(post => {
+        $postholder.append(`
                     <div class="row">
                         <a href="/posts/?post=${post.filename}" class="post">
                             <div>
                                 <ul>
-                                    <h2>${post.title}</h2>
-                                    <p>${post.date}</p>
-                                    <p>${post.description}</p>
+                                    <h2 class="post-title" >${post.title}</h2>
+                                    <p class="post-date">${post.date}</p>
+                                    <p class="post-description">${
+                                      post.description
+                                    }</p>
                                 </ul>
                             </div>
                         </a>
                     </div>
                 `);
-                postsOnPage++;
-            });
-        },
-        error: function() {
-            alert('Error: Failed to load posts!');
-        }
-    });
+        postsOnPage++;
+      });
+    },
+    error: function() {
+      alert("Error: Failed to load posts!");
+    }
+  });
 }
